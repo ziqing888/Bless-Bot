d100788b52f8aae529d2c4d30641db83365b9cac
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-15 01:57:20 +0800

    Update tokenInfo.js

--- tokenInfo.js
+++ tokenInfo.js
@@ -1,4 +1,7 @@
+// tokenInfo.js
 module.exports = {
-  nodeId: "你的节点ID",
-  authToken: "你的认证令牌"
+  nodeId: '12D3KooWFW8Xb17gN4ryJhNUUzznJARoPLm11LEyheNjr6wNQwWD',  // 填入你的节点 ID
+  hardwareId: 'your_hardware_id',  // 填入你的硬件 ID
+  bearerToken: 'your_bearer_token'  // 填入你的 Bearer Token
 };
+

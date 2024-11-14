fd183a9bcd68914e792a740cb6a07cff2420947c
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-15 02:51:14 +0800

    Delete tokenInfo.js

--- tokenInfo.js
+++
@@ -1,7 +0,0 @@
-// tokenInfo.js
-module.exports = {
-  nodeId: '12D3KooWFW8Xb17gN4ryJhNUUzznJARoPLm11LEyheNjr6wNQwWD',  // 填入你的节点 ID
-  hardwareId: 'your_hardware_id',  // 填入你的硬件 ID
-  bearerToken: 'your_bearer_token'  // 填入你的 Bearer Token
-};
-

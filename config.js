bfcd2859104d60dbfe59b5af456767162df3eda1
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-17 10:07:06 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        usertoken: 'usertoken1',
+        nodes: [
+            { nodeId: 'nodeid1', hardwareId: 'hardwareid1', proxy: 'proxy1' },
+            { nodeId: 'nodeid2', hardwareId: 'hardwareid2', proxy: 'proxy2' },
+            { nodeId: 'nodeid3', hardwareId: 'hardwareid3', proxy: 'proxy3' },
+            { nodeId: 'nodeid4', hardwareId: 'hardwareid4', proxy: 'proxy4' },
+            { nodeId: 'nodeid5', hardwareId: 'hardwareid5', proxy: 'proxy5' }
+        ]
+    },
+    {
+        usertoken: 'usertoken2',
+        nodes: [
+            { nodeId: 'nodeid6', hardwareId: 'hardwareid6', proxy: 'proxy6' },
+            { nodeId: 'nodeid7', hardwareId: 'hardwareid7', proxy: 'proxy7' },
+            { nodeId: 'nodeid8', hardwareId: 'hardwareid8', proxy: 'proxy8' },
+            { nodeId: 'nodeid9', hardwareId: 'hardwareid9', proxy: 'proxy9' },
+            { nodeId: 'nodeid10', hardwareId: 'hardwareid10', proxy: 'proxy10' }
+        ]
+    }
+    // Add more usertokens as needed
+];
